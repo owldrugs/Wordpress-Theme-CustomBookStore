adc31567885dff799e25ab6de0a6752da43c4371
Viktor <108957924+owldrugs@users.noreply.github.com>
2024-03-06 20:46:33 +0300

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,12 @@
+// jQuery(function($) {
+//     $( ".ajax_add_to_cart" ).on( "click", function() {
+//         alert( "Handler for `click` called." );
+//     } );
+// });
+// const ajaxUpdateCartContainer = document.querySelector('div.woocommerce');
+// if (ajaxUpdateCartContainer){
+//     ajaxUpdateCartContainer.addEventListener('change', ({ target }) => {
+//         target.closest('input.qty');
+//         document.querySelector('[name="update_cart"]').click();
+//     });
+// }
